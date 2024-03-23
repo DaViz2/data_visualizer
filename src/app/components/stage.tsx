e0c5d814c9acc8bd157e459a5fea0ae8f1264b44
loftol <60003939+loftol@users.noreply.github.com>
2024-03-23 16:38:45 +0900

    divide partition

---
+++ src/app/components/stage.tsx
@@ -0,0 +1,7 @@
+export default function Stage() {
+  return (
+    <div className="flex w-full h-full">
+      <div className="flex grow-[1] h-[100%] flex-col items-center p-24 bg-slate-100"/>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import {Userarea, Stage} from "./components/components";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex min-h-screen flex-col items-center justify-between p-24"/>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24"/>
+    <main className="flex h-[100vh] flex-row items-center p-5">
+      <div className="flex h-[100%] grow-[1] items-center p-5 bg-slate-300">
+        <Userarea/>
+      </div>
+      <div className="flex h-[100%] grow-[2] items-center p-5 bg-slate-400">
+        <Stage/>
+      </div>
     </main>
   );
 }
