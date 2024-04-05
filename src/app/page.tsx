c5182d421cc9e6d777e0c5361d4a1f45c4a257b6
Toyoill <61874428+Toyoill@users.noreply.github.com>
2024-04-05 15:32:28 +0900

    Fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Graph nodes={testJson.nodes} edges={testJson.edges} />
       <Draggable>
-        <Graph nodes={testJson.nodes} edges={testJson.edges} />
         <div>
           <DataTable
             headers={headers}
