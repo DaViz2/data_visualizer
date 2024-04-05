ba05a046192af8308c0cfa19075c52bbc0c014ce
Toyoill <61874428+Toyoill@users.noreply.github.com>
2024-04-05 15:22:48 +0900

    Update Graph component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
 'use client'
-
+import React from 'react';
 import Image from "next/image";
-import Circle from "./stage/component/Circle";
 import Draggable from "react-draggable";
+import Graph from "./stage/component/graph/Graph";
+
+import testJson from "../data/testgraph.json";
 
 export default function Home() {
-  const nodes = [1, 2, 3].map((val, index)=>
-    <Draggable>
-      <div>
-        <Circle key={index} value={val} />
-      </div> 
-    </Draggable>
-  )
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {nodes}
+      <Graph nodes={testJson.nodes} edges={testJson.edges}  />
     </main>
   );
 }
